14cde7096baba68a1e313568f4de0fd725168809
Josh Holladay <josh@jcreateit.com>
2018-05-17 09:42:36 -0600

    Feat (style) header, organized file tree

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './styles/main.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="App-header flex-all-center column">
+
+          <h1 className="App-title">Hi! I'm Josh Holladay.</h1>
+          <p className="App-intro">
+            I am a UX Designer. I code too.
+          </p>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
 }
 
 export default App;
+// <img src={logo} className="App-logo" alt="logo" />

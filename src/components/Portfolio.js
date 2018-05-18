09740c49413aecfc21201c0dad9f1aa231cfe93a
Josh Holladay <josh@jcreateit.com>
2018-05-18 09:08:09 -0600

    Feat (style) style portfolio, add images

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
 import box from '../assets/images/portfolio/box.png'
+import ppl from '../assets/images/portfolio/ppl.png'
+import emergent from '../assets/images/portfolio/EmergentSolutions.png'
+import coinless from '../assets/images/portfolio/Coinless.png'
+import lowder from '../assets/images/portfolio/LowderMusic.png'
+import strongbod from '../assets/images/portfolio/StrongBod.png'
+import upss from '../assets/images/portfolio/UPSS.png'
+import jcreateit from '../assets/images/portfolio/jcreateit.png'
+import crypto from '../assets/images/portfolio/cryptoelevation.png'
 
 class Portfolio extends Component {
   render() {
@@ -10,7 +18,19 @@ class Portfolio extends Component {
         </p>
         <div className="portfolio-container">
           <div class="box-container">
-            <img className="box" src={box} alt="box" />
+            <img className="box" src={jcreateit} alt="box" />
+            <div className="overlay">
+              <div className="text">Project Details</div>
+            </div>
+          </div>
+          <div class="box-container">
+            <img className="box" src={strongbod} alt="box" />
+            <div className="overlay">
+              <div className="text">Project Details</div>
+            </div>
+          </div>
+          <div class="box-container">
+            <img className="box" src={ppl} alt="box" />
             <div className="overlay">
               <div className="text">Project Details</div>
             </div>
@@ -22,61 +42,31 @@ class Portfolio extends Component {
             </div>
           </div>
           <div class="box-container">
-            <img className="box" src={box} alt="box" />
+            <img className="box" src={emergent} alt="box" />
             <div className="overlay">
               <div className="text">Project Details</div>
             </div>
           </div>
           <div class="box-container">
-            <img className="box" src={box} alt="box" />
+            <img className="box" src={lowder} alt="box" />
             <div className="overlay">
               <div className="text">Project Details</div>
             </div>
           </div>
           <div class="box-container">
-            <img className="box" src={box} alt="box" />
+            <img className="box" src={coinless} alt="box" />
             <div className="overlay">
               <div className="text">Project Details</div>
             </div>
           </div>
           <div class="box-container">
-            <img className="box" src={box} alt="box" />
+            <img className="box" src={upss} alt="box" />
             <div className="overlay">
               <div className="text">Project Details</div>
             </div>
           </div>
           <div class="box-container">
-            <img className="box" src={box} alt="box" />
-            <div className="overlay">
-              <div className="text">Project Details</div>
-            </div>
-          </div>
-          <div class="box-container">
-            <img className="box" src={box} alt="box" />
-            <div className="overlay">
-              <div className="text">Project Details</div>
-            </div>
-          </div>
-          <div class="box-container">
-            <img className="box" src={box} alt="box" />
-            <div className="overlay">
-              <div className="text">Project Details</div>
-            </div>
-          </div>
-          <div class="box-container">
-            <img className="box" src={box} alt="box" />
-            <div className="overlay">
-              <div className="text">Project Details</div>
-            </div>
-          </div>
-          <div class="box-container">
-            <img className="box" src={box} alt="box" />
-            <div className="overlay">
-              <div className="text">Project Details</div>
-            </div>
-          </div>
-          <div class="box-container">
-            <img className="box" src={box} alt="box" />
+            <img className="box" src={crypto} alt="box" />
             <div className="overlay">
               <div className="text">Project Details</div>
             </div>

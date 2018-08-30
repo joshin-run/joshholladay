2e1e208603e95d77857afd518752f1c7bb608b2b
Josh Holladay <josh@jcreateit.com>
2018-08-29 20:07:33 -0600

    Feat (style) Type face, drop shadow, add Marketing Banner

--- src/components/CaseStudies/ClearRTB-marketing.js
+++ src/components/CaseStudies/ClearRTB-marketing.js
@@ -3,7 +3,7 @@ import { HashRouter as Router, Route, Link } from 'react-router-dom'
 import Navbar from '../Navbar'
 import wall1 from '../../assets/images/portfolio/Case Studies/ppl/PPL-initialSketches.png'
 
-import CRTBMarketingBanner from '../../assets/images/CaseStudies/ClearRTB-Marketing/CRTB-Marketing-Banner.png'
+import CRTBMarketingBanner from '../../assets/images/CaseStudies/ClearRTB-Marketing/CRTB-Marketing-Banner2.png'
 import ClearRTBMarketingImg from '../../assets/images/CaseStudies/ClearRTB-Marketing/ClearRTB-Marketing-ProjectImage.png'
 
 import RealPicture1 from '../../assets/images/CaseStudies/ClearRTB-Marketing/Ideation/RealPicture-1.png'
@@ -41,13 +41,10 @@ const ClearRTBMarketing = (props) => (
     <Router>
       <div id="boilder-plate">
         <Navbar />
-        <div className="boilerPlateBanner">
-          <p className="banner-header">
-            ClearRTB Marketing Website
-          </p>
-        </div>
+        <img className="study-banner" src={CRTBMarketingBanner} />
+
         <div className="case-study-content-wrapper">
-        <img className="initial-sketches proj-main" src={ClearRTBMarketingImg} />
+
           <div className="section-container">
             <p className="sect-head">Company Bio: Why. What. How.</p><br />
             <p className="section-content">Text</p>
@@ -93,45 +90,45 @@ const ClearRTBMarketing = (props) => (
 
             <div>
               <p className="sub-head">The Real Picture</p>
-              <img src={RealPicture1} alt="box" />
+              <img className="shadow" src={RealPicture1} alt="box" />
               <p className="section-content">As a UX Designer, my first priority was to meet with the stake holders to understand the problems and goals for the projects. When we met next I went through a series of questions that would help to focus our efforts and ensure that the product we were about to built would solve the problems at hand for the user.</p><br />
-              <img src={RealPicture2} alt="box" />
+              <img className="shadow" src={RealPicture2} alt="box" />
               <p className="section-content">As a UX Designer, my first priority was to meet with the stake holders to understand the problems and goals for the projects. When we met next I went through a series of questions that would help to focus our efforts and ensure that the product we were about to built would solve the problems at hand for the user.</p><br />
             </div>
 
             <div>
               <p className="sub-head">Blur to Clear</p>
-              <img src={BlurToClear1} alt="box" />
+              <img className="shadow" src={BlurToClear1} alt="box" />
               <p className="section-content">As a UX Designer, my first priority was to meet with the stake holders to understand the problems and goals for the projects. When we met next I went through a series of questions that would help to focus our efforts and ensure that the product we were about to built would solve the problems at hand for the user.</p><br />
-              <img src={BlurToClear2} alt="box" />
+              <img className="shadow" src={BlurToClear2} alt="box" />
               <p className="section-content">As a UX Designer, my first priority was to meet with the stake holders to understand the problems and goals for the projects. When we met next I went through a series of questions that would help to focus our efforts and ensure that the product we were about to built would solve the problems at hand for the user.</p><br />
-              <img src={BlurToClear3} alt="box" />
+              <img className="shadow" src={BlurToClear3} alt="box" />
               <p className="section-content">As a UX Designer, my first priority was to meet with the stake holders to understand the problems and goals for the projects. When we met next I went through a series of questions that would help to focus our efforts and ensure that the product we were about to built would solve the problems at hand for the user.</p><br />
-              <img src={BlurToClear4} alt="box" />
+              <img className="shadow" src={BlurToClear4} alt="box" />
               <p className="section-content">As a UX Designer, my first priority was to meet with the stake holders to understand the problems and goals for the projects. When we met next I went through a series of questions that would help to focus our efforts and ensure that the product we were about to built would solve the problems at hand for the user.</p><br />
             </div>
 
             <div>
               <p className="sub-head">Blue Blur</p>
-              <img src={BlueBlur} alt="box" />
+              <img className="shadow" src={BlueBlur} alt="box" />
               <p className="section-content">As a UX Designer, my first priority was to meet with the stake holders to understand the problems and goals for the projects. When we met next I went through a series of questions that would help to focus our efforts and ensure that the product we were about to built would solve the problems at hand for the user.</p><br />
             </div>
 
             <div>
               <p className="sub-head">Dark Side of the Moon</p>
-              <img src={Moon1} alt="box" />
+              <img className="shadow" src={Moon1} alt="box" />
               <p className="section-content">As a UX Designer, my first priority was to meet with the stake holders to understand the problems and goals for the projects. When we met next I went through a series of questions that would help to focus our efforts and ensure that the product we were about to built would solve the problems at hand for the user.</p><br />
-              <img src={Moon2} alt="box" />
+              <img className="shadow" src={Moon2} alt="box" />
               <p className="section-content">As a UX Designer, my first priority was to meet with the stake holders to understand the problems and goals for the projects. When we met next I went through a series of questions that would help to focus our efforts and ensure that the product we were about to built would solve the problems at hand for the user.</p><br />
-              <img src={Moon3} alt="box" />
+              <img className="shadow" src={Moon3} alt="box" />
               <p className="section-content">As a UX Designer, my first priority was to meet with the stake holders to understand the problems and goals for the projects. When we met next I went through a series of questions that would help to focus our efforts and ensure that the product we were about to built would solve the problems at hand for the user.</p><br />
             </div>
 
             <div>
               <p className="sub-head">Dark Side of the Moon</p>
-              <img src={MeetMike} alt="box" />
+              <img className="shadow" src={MeetMike} alt="box" />
               <p className="section-content">As a UX Designer, my first priority was to meet with the stake holders to understand the problems and goals for the projects. When we met next I went through a series of questions that would help to focus our efforts and ensure that the product we were about to built would solve the problems at hand for the user.</p><br />
-              <img src={Meet3} alt="box" />
+              <img className="shadow" src={Meet3} alt="box" />
               <p className="section-content">As a UX Designer, my first priority was to meet with the stake holders to understand the problems and goals for the projects. When we met next I went through a series of questions that would help to focus our efforts and ensure that the product we were about to built would solve the problems at hand for the user.</p><br />
             </div>
 
@@ -145,7 +142,7 @@ const ClearRTBMarketing = (props) => (
 
             <div>
               <p className="sub-head">The Inspiration</p>
-              <img src={encOriginal} alt="box" />
+              <img className="shadow" src={encOriginal} alt="box" />
               <p className="section-content">I found this picture of a king galivanting around which reflected the ideas of the company.</p><br />
             </div>
 
